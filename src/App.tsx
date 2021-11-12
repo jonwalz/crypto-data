ba417062adddec981d4b1de9464bffb1d064f1af
Jon <jon.walz.music@gmail.com>
2021-11-12 11:12:34 -0800

    setup example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createClient, Provider, useQuery } from "urql";
+import "./App.css";
+
+const CURRENCIES_EXAMPLE = `
+  query {
+    allCurrencyProjects(page: 1) {
+      symbol
+      ticker
+      name
+      priceUsd
+      volumeChange24h
+      marketcapUsd
+    }
+  }
+`;
+
+const client = createClient({
+  url: "https://api.santiment.net/graphql",
+});
+
+function Currencies() {
+  // useQuery will make a POST request with CURRENCIES_EXAMPLE as the body
+  const [result] = useQuery({ query: CURRENCIES_EXAMPLE });
+
+  const { data, fetching, error } = result;
+
+  // 'fetching' is a common pattern indicating a loading state
+  if (fetching) return <div>Loading...</div>;
+
+  if (error) return <div>Whoops, something bad happened...</div>;
+
+  // Check the console to see data shape
+  console.log("DATA: ", data.allCurrencyProjects);
+
+  return (
+    <ul>
+      {data.allCurrencyProjects.map((currency, i) => {
+        return (
+          <li key={`${currency.name}+${i}`}>
+            <div>Name: {currency.name}</div>
+            <div>Currency price: {currency.priceUsd}</div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider value={client}>
+      <Currencies />
+    </Provider>
   );
 }
 
