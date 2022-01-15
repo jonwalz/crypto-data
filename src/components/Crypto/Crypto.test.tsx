cdf97dccea40d3acc1842c68df57dfeaaf6f2953
Jon Walz <jon.walz.music@gmail.com>
2022-01-15 09:56:31 -0800

    Update tests. Remove unused component

--- src/components/Crypto/Crypto.test.tsx
+++ src/components/Crypto/Crypto.test.tsx
@@ -2,18 +2,16 @@ import { render, screen } from '@testing-library/react'
 import { CryptoCard } from './'
 
 const props = {
-  name: 'Test',
+  name: 'Test name',
   i: 0,
-  priceUsd: '1',
+  priceUsd: '123',
   symbol: '',
 }
 describe('Crypto component', () => {
   it('Should render correctly', () => {
     render(<CryptoCard {...props} />)
 
-    // screen.debug() will print the dom to the terminal
-    // screen.debug();
-    screen.getByText(/Name: test/i) // '/Name: test/i' is regex for incomplete match
-    screen.getByText(/Currency price: 1/i)
+    screen.getByText(/Test name/i) // '/Name: test/i' is regex for incomplete match
+    screen.getByText(/123/i)
   })
 })
--- src/components/Currencies/styles.tsx
+++
@@ -1,7 +0,0 @@
-import { styled } from '@stitches/react'
-
-export const Box = styled('div', {
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-})
