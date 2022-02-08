c33539bb24a0a90333496c27f4a7dbd55caf67b0
Jon Walz <jon.walz.music@gmail.com>
2022-02-07 21:28:29 -0800

    Update color scheme. Update routes

--- app/theme.ts
+++ app/theme.ts
@@ -1,18 +1,23 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const colors = {
-  blue: {
-    400: "#275F6A",
-    500: "#4F94A1",
-    600: "#397985",
-    700: "#194850",
-    800: "#0D2F36",
+  green: {
+    50: "#e1fafc",
+    100: "#c2e9ec",
+    200: "#a1d8dc",
+    300: "#7ec9cd",
+    400: "#5cb9be",
+    500: "#44a0a6",
+    600: "#327c81",
+    700: "#21595c",
+    800: "#0c3638",
+    900: "#001414",
   },
 };
 
 const global = {
   body: {
-    color: "white",
+    color: "green.300",
   },
 };
 
