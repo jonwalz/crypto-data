3b874db80f898e49f9d61a71d90c3db6a4235da8
jwswiftly <jw@jonathanwalz.com>
2023-02-26 15:29:44 -0800

    streamline faker user

--- app/utils/sign-in.ts
+++ app/utils/sign-in.ts
@@ -1,51 +1,51 @@
-import { createUserSession, register } from '~/session.server'
-import { v4 as uuidv4 } from 'uuid'
-import { User } from '~/models'
-import { json, redirect } from 'remix'
-import { getUser, verifyLogin } from '~/db.server'
+import { createUserSession, register } from "~/session.server";
+import { v4 as uuidv4 } from "uuid";
+import { User } from "~/models";
+import { json, redirect } from "remix";
+import { getUser, verifyLogin } from "~/db.server";
 
 export function handleCreateUser(address: string) {
-  const nonce = uuidv4()
+  const nonce = uuidv4();
 
   return register({
     publicAddress: String(address),
     nonce,
-    opts: { redirect: '/screener' },
-  })
+    opts: { redirect: "/screener" },
+  });
 }
 
 export const handleAttemptLogin = async (
   address: string,
-  redirectTo: string = '/screener',
+  redirectTo: string = "/screener"
 ) => {
-  let user: User | null
+  if (address === "faker") {
+    return await createUserSession("faker", {
+      redirect: redirectTo || "/screener",
+    });
+  }
+  let user: User | null;
   try {
-    user = await verifyLogin(address)
+    user = await verifyLogin(address);
   } catch (e) {
-    let formError: string
+    let formError: string;
     if (e instanceof Error) {
-      formError = e.message + '!!!'
-    } else if (typeof e === 'string') {
-      formError = e
+      formError = e.message + "!!!";
+    } else if (typeof e === "string") {
+      formError = e;
     } else {
-      formError = 'There was an error logging in. Please try again later.'
+      formError = "There was an error logging in. Please try again later.";
     }
 
-    console.log('HERE')
-    return handleCreateUser(address)
+    return handleCreateUser(address);
   }
-
-  return await createUserSession(user.id, {
-    redirect: redirectTo || '/screener',
-  })
-}
+};
 
 interface ActionData {
-  formError?: string
-  fieldErrors?: FieldErrors
-  fields?: Record<TextFields, string>
+  formError?: string;
+  fieldErrors?: FieldErrors;
+  fields?: Record<TextFields, string>;
 }
 
-type FieldErrors = Record<TextFields, string | null>
+type FieldErrors = Record<TextFields, string | null>;
 
-type TextFields = 'address'
+type TextFields = "address";
